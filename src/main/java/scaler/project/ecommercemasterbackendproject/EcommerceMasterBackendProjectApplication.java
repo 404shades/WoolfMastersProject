2892e513ced9f8ba1b471ead97f7befcf425ba5d
Rohan Malik <imrohanmalik@gmail.com>
2024-04-28 10:00:00 +0530

    Initial setup for Spring Boot project

---
+++ src/main/java/scaler/project/ecommercemasterbackendproject/EcommerceMasterBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package scaler.project.ecommercemasterbackendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMasterBackendProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMasterBackendProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scaler/project/ecommercemasterbackendproject/EcommerceMasterBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package scaler.project.ecommercemasterbackendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMasterBackendProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
